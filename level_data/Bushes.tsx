58fe91306b5c4a475054e720e22937f2c73625bb
Vulaidung <dungvulai2007@gmail.com>
2023-08-08 23:20:04 +0700

    adding sound

--- level_data/Bushes.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="6" tilewidth="16" tileheight="16" tilecount="2" columns="2">
- <image source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Bushes/6.png" width="32" height="25"/>
-</tileset>
