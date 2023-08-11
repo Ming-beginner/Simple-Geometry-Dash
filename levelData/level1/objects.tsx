8af88175d525d7c44dd96314e7233b9a8f3a077e
Vulaidung <dungvulai2007@gmail.com>
2023-08-11 22:39:45 +0700

    adding image

---
+++ levelData/level1/objects.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="370" tileheight="600" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="25" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Bushes/8.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="25" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Bushes/7.png"/>
+ </tile>
+ <tile id="2">
+  <image width="62" height="103" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="200" height="200" source="../pygame.graphics/Black triangle.png"/>
+ </tile>
+ <tile id="4">
+  <image width="370" height="600" source="portal.png"/>
+ </tile>
+ <tile id="5">
+  <image width="7" height="5" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Grass/8.png"/>
+ </tile>
+ <tile id="6">
+  <image width="7" height="7" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Grass/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="7" height="5" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Grass/6.png"/>
+ </tile>
+ <tile id="8">
+  <image width="7" height="6" source="../pygame.graphics/craftpix-net-753737-free-green-zone-tileset-pixel-art/3 Objects/Grass/15.png"/>
+ </tile>
+</tileset>
