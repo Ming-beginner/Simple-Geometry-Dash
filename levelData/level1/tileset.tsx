923a7c467af289bfba8e6e670621daf1eb989985
Ming-beginner <lengocminh310808@gmail.com>
2023-08-24 15:15:07 +0700

    update level 1

---
+++ levelData/level1/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Tileset" tilewidth="16" tileheight="16" tilecount="324" columns="18">
+ <image source="E:/Ming/code/mindx/CSB/levelData/level1/tileset/Tileset.png" width="288" height="288"/>
+</tileset>
